fdb1cae8a551b7acd68ed18830c7acfa6993a8c2
Andres Rivero <andres19rivero@gmail.com>
2024-08-18 18:34:11 -0400

    feat(todos): impl getTodoById method

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,13 +1,28 @@
 import { Request, Response } from "express";
 
+const todos = [
+  { id: 1, text: "Buy milk", createdAt: new Date() },
+  { id: 2, text: "Buy bread", createdAt: null },
+  { id: 3, text: "Buy butter", createdAt: new Date() },
+];
+
 export class TodosController {
   constructor() {}
 
   public getTodos = (req: Request, res: Response) => {
-    return res.json([
-      { id: 1, text: "Buy milk", createdAt: new Date() },
-      { id: 2, text: "Buy bread", createdAt: null },
-      { id: 3, text: "Buy butter", createdAt: new Date() },
-    ]);
+    return res.json(todos);
+  };
+
+  public getTodoById = (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    if (isNaN(id))
+      return res.status(400).json({ error: "ID argument is not a number" });
+
+    const todo = todos.find((todo) => todo.id === id);
+
+    if (!todo)
+      return res.status(404).json({ error: `TODO with id ${id} not found` });
+
+    return res.json(todo);
   };
 }

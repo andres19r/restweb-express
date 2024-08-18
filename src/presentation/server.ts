e559340d05bc23a5d907f88ef9bcd922a01aa7f7
Andres Rivero <andres19rivero@gmail.com>
2024-08-18 18:16:25 -0400

    feat: impl app routes & inject envvars to server object

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,13 +1,33 @@
-import express from "express";
+import express, { Router } from "express";
+
+interface Options {
+  port: number;
+  routes: Router;
+  public_path?: string;
+}
 
 export class Server {
   private app = express();
+  private readonly port: number;
+  private readonly publicPath: string;
+  private readonly routes: Router;
+
+  constructor(options: Options) {
+    const { port, routes, public_path = "public" } = options;
+    this.port = port;
+    this.publicPath = public_path;
+    this.routes = routes;
+  }
 
   async start() {
-    this.app.use(express.static('public'));
+    // Publi folder
+    this.app.use(express.static(this.publicPath));
 
-    this.app.listen(3000, () => {
-      console.log(`Server running on port ${3000}`);
+    // Routes
+    this.app.use(this.routes);
+
+    this.app.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
     });
   }
 }
